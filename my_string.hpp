0f61f042950363730c2cbaed842dec8cb39c34d1
VardGH <varduhi.ghukasy@gmail.com>
2023-05-21 23:54:24 +0400

    add my_string.hpp

---
+++ my_string.hpp
@@ -0,0 +1,29 @@
+#ifndef MY_STRIING_HPP
+#define MY_STRIING_HPP
+
+class my_string
+{
+public:
+    my_string();
+    my_string(const char* str);
+    my_string(const my_string& other);
+    my_string(my_string&& other) noexcept;
+    my_string& operator=(const my_string& other);
+    my_string& operator=(my_string&&) noexcept;
+    ~my_string();
+
+public:
+    char& operator[](int index);
+    const char& operator[](int index) const;
+    my_string& operator+(const my_string& other) const;
+    my_string& operator+(const std::string& str) const;
+    my_string& operator+(const char* str) const;
+    const char* c_str() const;
+    int size() const;
+
+private:
+    int m_cap;
+    char* m_buf;
+};
+
+#endif // MY_STRIING_HPP
